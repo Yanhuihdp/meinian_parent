f5c84fe4282a67988ee7bb022c8b0504c1dff97e
闫辉 <317201396@qq.com>
2022-03-25 13:37:36 +0800

    修改ABC_AAAAAA

--- meinian_common/src/main/java/com/atguigu/constant/ABC.java
+++ meinian_common/src/main/java/com/atguigu/constant/ABC.java
@@ -6,6 +6,6 @@ package com.atguigu.constant;
  */
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("1111111111111");
+        System.out.println("1111111111111AAAAAA");
     }
 }
