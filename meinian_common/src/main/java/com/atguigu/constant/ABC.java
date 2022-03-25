1ae7785cc5d729e30096fdc12f9f28a56853d42a
闫辉 <317201396@qq.com>
2022-03-25 13:36:10 +0800

    新增ABC

---
+++ meinian_common/src/main/java/com/atguigu/constant/ABC.java
@@ -0,0 +1,11 @@
+package com.atguigu.constant;
+
+/**
+ * @author shkstart
+ * @create 2022-03-25 13:35
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+    }
+}
